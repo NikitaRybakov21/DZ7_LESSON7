91c097fef19adc98f4798b8086a838714dd7956f
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-07-14 15:24:33 +0300

    DZ7_Lesson7

--- src/main/java/github/Cat.java
+++ src/main/java/github/Cat.java
@@ -1,5 +1,18 @@
 package github;
 
 public class Cat{
-   
+    private String name;
+    private int appetite;
+
+    public boolean FullTank;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.FullTank = false;
+    }
+
+    public void eat(Plate p) {
+      FullTank = p.decreaseFood(appetite);
+    }
 }
--- src/main/java/github/Plate.java
+++ src/main/java/github/Plate.java
@@ -1,5 +1,23 @@
 package github;
 
 public class Plate {
+    private int food;
 
+    public Plate(int food) {
+        this.food = food;
+    }
+    public boolean decreaseFood(int n) {
+        if(food - n >= 0){
+            food -= n;
+            return true;
+        }
+        return false;
+    }
+    public void info(Boolean full) {
+        if(full) System.out.println("plate: " + food + " кот сыт");
+        else  System.out.println("plate: " + food + " кот голодный");
+    }
+    public void reFuel(int fuel){
+        food += fuel;
+    }
 }
